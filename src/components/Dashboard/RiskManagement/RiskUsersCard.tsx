0a8f1fb3c8ce654bc89d600e5a35f1df11c3ae58
Apple <apple@Apples-MacBook-Air.local>
2025-09-04 00:39:28 +0100

    Risk User page done

--- src/components/Dashboard/RiskManagement/RiskUsersCard.tsx
+++ src/components/Dashboard/RiskManagement/RiskUsersCard.tsx
@@ -139,7 +139,7 @@ const RiskUsersCard = () => {
                                 Last Login Location :
                             </p>
                             <div className="flex items-center gap-2">
-                                <div className="w-2 h-2 bg-green-500 rounded-full"></div>
+                                <div className="w-2 h-2 bg-green-500 rounded-full animate-pulse"></div>
                                 <span className="text-xs sm:text-sm text-gray-700 dark:text-gray-300 break-all">
                                     {user.location}
                                 </span>
